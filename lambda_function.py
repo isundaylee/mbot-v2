5b924689fbf64e859ed388f4579342ad25046ce0
Jiahao Li <reg@ljh.me>
2021-01-16 12:18:23 -0500

    Fixes request data access

--- lambda_function.py
+++ lambda_function.py
@@ -10,17 +10,19 @@ from mbot.messenger import MessengerClient
 
 @lambda_handler.handle("post", path="/relay")
 def do_relay(event):
+    data = event["json"]["body"]
+
     if (
-        ("secret" not in event)
+        ("secret" not in data)
         or ("MBOT_RELAY_SECRET" not in os.environ)
-        or (event["secret"] != os.environ["MBOT_RELAY_SECRET"])
+        or (data["secret"] != os.environ["MBOT_RELAY_SECRET"])
     ):
         return Response(
             body={"success": False, "error": "No valid secret given."}, status_code=403
         )
 
     MessengerClient(access_token=os.environ["FB_PAGE_ACCESS_TOKEN"]).send_message(
-        os.environ["FB_OWNER_ID"], event["message"]
+        os.environ["FB_OWNER_ID"], data["message"]
     )
 
     return {"success": True}
