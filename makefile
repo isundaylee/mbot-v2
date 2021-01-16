7644179f0a55c55fcbe5f3df9eb5e42dd5fd7543
Jiahao Li <reg@ljh.me>
2021-01-16 11:45:30 -0500

    relay: Makes it work for Lambda Proxy

--- makefile
+++ makefile
@@ -1,5 +1,7 @@
 dist/mbot-relay.zip: mbot-relay/lambda_function.py
-	cd mbot-relay; zip -r ../dist/mbot-relay.zip *
+	cd mbot-relay; pip install --target ./packages -r requirements.txt
+	cd mbot-relay/packages; zip -r ../../dist/mbot-relay.zip *
+	cd mbot-relay/; zip -r ../dist/mbot-relay.zip lambda_function.py
 
 deploy-mbot-relay: dist/mbot-relay.zip
 	aws lambda update-function-code --region us-east-1 --function-name mbot-relay --zip-file fileb://dist/mbot-relay.zip
--- mbot-relay/lambda_function.py
+++ mbot-relay/lambda_function.py
@@ -1,5 +1,43 @@
+import os
 import json
+import logging
+
+import requests
+
+
+def send_message_to_owner(message):
+    logging.info("Sending message: {}".format(message))
+    request_body = {
+        "recipient": {"id": os.environ.get("FB_OWNER_ID")},
+        "message": {"text": message},
+    }
+
+    address = "https://graph.facebook.com/v2.6/me/messages?access_token={}".format(
+        os.environ.get("FB_PAGE_ACCESS_TOKEN")
+    )
+    resp = requests.post(address, json=request_body)
+
+    logging.info("Response: {} {}".format(resp.status_code, resp.json()))
+
+
+def make_response(payload, status_code):
+    return {
+        "isBase64Encoded": False,
+        "statusCode": status_code,
+        "headers": {},
+        "body": json.dumps(payload),
+    }
 
 
 def lambda_handler(event, context):
-    return {"success": True}
+    if (
+        ("secret" not in event)
+        or ("MBOT_RELAY_SECRET" not in os.environ)
+        or (event["secret"] != os.environ["MBOT_RELAY_SECRET"])
+    ):
+        return make_response({"success": False, "error": "No valid secret given."}, 403)
+
+    send_message_to_owner(event["message"])
+
+    return make_response({"success": True}, 200)
+
