9a818d747871c9b7d727ee81a191cb487a353c95
Jiahao Li <reg@ljh.me>
2021-10-16 22:10:00 -0400

    Increases row to 3000

--- mbot/utils/google_apps.py
+++ mbot/utils/google_apps.py
@@ -31,7 +31,7 @@ class GoogleApps:
             "sheets", "v4", http=self.http, discoveryServiceUrl=discoveryURL
         )
 
-        total_rows = 2000
+        total_rows = 3000
 
         vacant_row = None
         result = (
