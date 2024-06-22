eb30e5d437a627268dd2ad30c61e5ae615f149a9
Jiahao Li <reg@ljh.me>
2024-06-21 22:50:52 -0400

    Slack reply bot

--- mbot/handlers/slack.py
+++ mbot/handlers/slack.py
@@ -1,3 +1,4 @@
+import functools
 import json
 import logging
 import os
@@ -7,6 +8,28 @@ from slack_sdk import signature
 import slack_sdk
 
 logger = logging.getLogger(__name__)
+
+
+@functools.cache
+def _get_slack_client() -> slack_sdk.WebClient:
+    return slack_sdk.WebClient(os.environ["SLACK_BOT_USER_TOKEN"])
+
+
+def _handle_event(event: dict[str, Any]) -> None:
+    def _reply(message: str) -> None:
+        client = _get_slack_client()
+        client.chat_postMessage(
+            channel=event["channel"], thread_ts=event["ts"], text=message
+        )
+
+    match event_type := event["type"]:
+        case "message":
+            if "bot_id" in event:
+                return
+
+            _reply("Hello you")
+        case _:
+            raise RuntimeError(f"Invalid event type {event_type}")
 
 
 def do_slack_post(event: dict[str, Any]) -> Any:
@@ -24,6 +47,8 @@ def do_slack_post(event: dict[str, Any]) -> Any:
         case "url_verification":
             challenge = json_body["challenge"]
             return challenge
+        case "event_callback":
+            _handle_event(json_body["event"])
         case _:
             raise RuntimeError(f"Invalid request type {request_type}")
 
